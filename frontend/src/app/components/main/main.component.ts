9144e487e776e15192c74c1f0c07d530c90c455c
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2023-12-20 23:17:52 +0530

    auth and autho add

--- frontend/src/app/components/main/main.component.ts
+++ frontend/src/app/components/main/main.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { NgToastService } from 'ng-angular-popup';
 import { HttpServiceService } from 'src/app/database/http.service.service';
 import { CreateBlogComponent } from 'src/app/models/create-blog/create-blog.component';
+import { AuthServiceService } from 'src/app/shared/auth-service.service';
 
 @Component({
   selector: 'app-main',
@@ -26,68 +28,98 @@ export class MainComponent implements OnInit {
   constructor(
     public apiService: HttpServiceService,
     private matDialog: MatDialog,
-    private toaster: NgToastService
+    private toaster: NgToastService,
+    public authService: AuthServiceService,
+    private router: Router,
 
   ) {}
 
   createBlog(): void {
-    const dialogRef: MatDialogRef<CreateBlogComponent> = this.matDialog.open(
-      CreateBlogComponent,
-      {
-        width: '700px',
-        // height: '630px',
-        // maxWidth: '100%',
-        disableClose: true,
-        data: { message: 'Create' },
-        // height:'800px',
-      }
-    );
-    dialogRef.afterClosed().subscribe((result: any) => {
-      if (result && (result.saved || result.updated)) {
-        this.getAllBlogs()
-      }
-    });
+    if (this.authService.CurrentUserSig()) {
+      const dialogRef: MatDialogRef<CreateBlogComponent> = this.matDialog.open(
+        CreateBlogComponent,
+        {
+          width: '700px',
+          // height: '630px',
+          // maxWidth: '100%',
+          disableClose: true,
+          data: { message: 'Create' },
+          // height:'800px',
+        }
+      );
+      dialogRef.afterClosed().subscribe((result: any) => {
+        if (result && (result.saved || result.updated)) {
+          this.getAllBlogs();
+        }
+      });
+    } else {
+      this.router.navigateByUrl('/login');
+      this.toaster.info({
+        detail: 'Information',
+        summary: 'Please Login or Register access this blog',
+        duration: 5000,
+        position: 'topRight',
+      });
+    }
   }
 
   editBlog(blog: any): void {
-    const dialogRef: MatDialogRef<CreateBlogComponent> = this.matDialog.open(
-      CreateBlogComponent,
-      {
-        width: '700px',
-        // height: '630px',
-        // maxWidth: '100%',
-        disableClose: true,
-        data: { blog: blog, message: 'Create' },
-        // height:'800px',
-      }
-    );
-    dialogRef.afterClosed().subscribe((result: any) => {
-      if (result && (result.saved || result.updated)) {
-        this.getAllBlogs()
-      }
-    });
+    if (this.authService.CurrentUserSig()) {
+      const dialogRef: MatDialogRef<CreateBlogComponent> = this.matDialog.open(
+        CreateBlogComponent,
+        {
+          width: '700px',
+          // height: '630px',
+          // maxWidth: '100%',
+          disableClose: true,
+          data: { blog: blog, message: 'Create' },
+          // height:'800px',
+        }
+      );
+      dialogRef.afterClosed().subscribe((result: any) => {
+        if (result && (result.saved || result.updated)) {
+          this.getAllBlogs();
+        }
+      });
+    } else {
+      this.router.navigateByUrl('/login');
+      this.toaster.info({
+        detail: 'Information',
+        summary: 'Please Login or Register access this blog',
+        duration: 5000,
+        position: 'topRight',
+      });
+    }
   }
 
-  deleteBlog(blogId:any){
-    this.apiService.deleteData(`/api/blog/delete/${blogId}`).subscribe(
-      (response: any) => {
-        this.toaster.success({
-          detail: 'SUCCESS',
-          summary: 'Blog deleted successfully',
-          duration: 5000,
-          position: 'topRight',
-        });
-      
-      },
-      (error: any) => {
-  
-        this.toaster.error({
-          detail: 'ERROR',
-          summary: 'Failed to delete blog. Please try again.',
-          sticky: true,
-          position: 'topRight',
-        });
-      }
-    );
+  deleteBlog(blogId: any) {
+    if (this.authService.CurrentUserSig()) {
+      this.apiService.deleteData(`/api/blog/delete/${blogId}`).subscribe(
+        (response: any) => {
+          this.toaster.success({
+            detail: 'SUCCESS',
+            summary: 'Blog deleted successfully',
+            duration: 5000,
+            position: 'topRight',
+          });
+        },
+        (error: any) => {
+          this.toaster.error({
+            detail: 'ERROR',
+            summary: 'Failed to delete blog. Please try again.',
+            sticky: true,
+            position: 'topRight',
+          });
+        }
+      );
+    } else {
+      this.router.navigateByUrl('/login');
+      this.toaster.info({
+        detail: 'Information',
+        summary: 'Please Login or Register access this blog',
+        duration: 5000,
+        position: 'topRight',
+      });
+    }
   }
 }
